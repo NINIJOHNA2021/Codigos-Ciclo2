b900b696dd01d330ee21f0750939b16a1fcfeeb1
NINIJOHNA2021 <interjohana@gmail.com>
2021-08-07 12:40:19 -0500

    Se eliminó una linea

--- src/App.java
+++ src/App.java
@@ -4,7 +4,6 @@ public class App {
     Bloque de comentario
     */
     public static void main(String[] args)  {
-        System.out.println("Hello, World!");
         System.out.println("Hello, Castiel!");
 
 
