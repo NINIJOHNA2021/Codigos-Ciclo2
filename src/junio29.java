4becceb4d837e631fd5c83d28986b9266bdc5f0e
NINIJOHNA2021 <interjohana@gmail.com>
2021-08-07 12:15:51 -0500

    Resumen del día

---
+++ src/junio29.java
@@ -0,0 +1,86 @@
+public class junio29 {
+    
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+    System.out.println(" El mensaje fue " + entrada);
+    int resultado =scanner.next.nextInt ();
+    System.out.println(" El mensaje fue" resultados);
+    if (5>3){
+        System.out.println(":");}
+    else if (3>2){
+        System.out.println("T.T");
+    else {
+        System.out.println (":(")
+    }*/
+    //Scanner scanner = new Scanner (System.in);
+    //System.out.println( "Ingresa un animal");
+    //string animal = scanner.nexLine();
+    /*if (animal.equals("perro")){
+        System.out.println ("woff");
+    }else{
+        System.out,println("Miau");
+    else{
+        System.out.println("No encuentro el animal");
+
+    }*/
+    /*switch (animal){
+        case "perro":
+             System.out.println("woff");
+             break;
+         case "Gato":
+              system.out.println("Miau");
+              break;
+        case "Loro":
+            System.out.println("Quiere cacao?");
+            break;
+        default:
+             System.out.println("No se encuentra el animal");
+
+    int numero = scanner.nextInt();
+    switch(numero){
+        case 0:
+        System.out.println("Es igual a 0");
+        break;
+    default:
+        System.out.println(" No es igual a 0");
+}*/
+/*int contador = 1;
+while(true){
+    System.out.println(":)")";
+    contador =contador + 1;
+    if(contador==5){
+        break;
+    }
+}*/
+/*int numero = 5;
+do{
+    System.out.println(":)");
+}while(numero>7);*/
+//for i in range(0,5,1):
+/*for(int i=0;i <5;i=i+1){
+    System.out.println(i);
+}*/
+/*for(int i=7; i>0; i= i-1){
+    System.out.println(i);
+}*/
+/*for(int i=0;i<20;i=i+2){
+    System.out.println(i);
+}*/
+for(int i=0;i<=20;i=i+2){
+    if(i==8){
+       //continue;
+       break;
+    }
+    System.out.println(i);
+
+}
+
+ }    
+  
+    }
+
+    
+    
+     
+    
+}
---
+++ src/junio29c.java
@@ -0,0 +1,14 @@
+public class junio29c {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Ingresa tu Castiel");
+        String nombre = scanner.nextLine();
+        //System.out.println("Hola"+Castiel);
+        System.out.println(saludar(Castiel));
+    }
+
+    public static String  saludar(String Castiel){
+        return "Hola" + Castiel;
+    }
+
+    def ejemploFuncion(variable1:int)->float:
